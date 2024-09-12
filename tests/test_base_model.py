62718cc5eb19e358a4a11e7b1678ccfb21d655b4
pauline12ish34 <paulineishimwe12@gmail.com>
2024-09-12 21:34:48 +0200

    ft-blank line

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,4 +1,4 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
 # from models.base_model import BaseModel
 """Unittest module for the BaseModel Class."""
 
@@ -8,6 +8,7 @@ import os
 import time
 from io import StringIO
 from unittest.mock import patch
+
 
 class TestBaseModel(unittest.TestCase):
     """Test cases for the BaseModel."""
@@ -20,7 +21,8 @@ class TestBaseModel(unittest.TestCase):
     def test_class_type(self):
         """Test the class type of the instance."""
         base = BaseModel()
-        self.assertEqual(str(type(base)), "<class 'models.base_model.BaseModel'>")
+        self.assertEqual(str(type(base)),
+                         "<class 'models.base_model.BaseModel'>")
 
     def test_save(self):
         """Test the save method updates the updated_at attribute."""
@@ -41,7 +43,8 @@ class TestBaseModel(unittest.TestCase):
     def test_str_representation(self):
         """Test the string representation of the object."""
         base = BaseModel()
-        expected_output = f"[{base.__class__.__name__}] ({base.id}) {base.__dict__}\n"
+        expected_output = (f"[{base.__class__.__name__}] ({base.id}) "
+                           f"{base.__dict__}\n")
         with patch("sys.stdout", new=StringIO()) as fake_out:
             print(base)
             self.assertEqual(fake_out.getvalue(), expected_output)
@@ -51,6 +54,7 @@ class TestBaseModel(unittest.TestCase):
         base = BaseModel()
         dict_repr = base.to_dict()
         self.assertEqual(dict_repr['__class__'], base.__class__.__name__)
+
 
 if __name__ == "__main__":
     unittest.main()
