508fbed2e851670e9ebd4c776999d60a0db37f63
kellenmurerwa <kellenmurerwa724@gmail.com>
2024-09-12 19:46:26 +0200

    Added state model into console.py

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-# from models.state import State
+from models.state import State
 from models.city import City
 from models.place import Place
 # from models.amenity import Amenity
@@ -18,7 +18,7 @@ class HBNBCommand(cmd.Cmd):
     __all_classes = {
         "BaseModel",
         "User",
-        # "State",
+        "State",
         "City",
         # "Amenity",
         "Place",
